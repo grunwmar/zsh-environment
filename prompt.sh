a5377307f4c82f808f36e7c55437476237fc9936
martin grunwald <mgrunwald91@gmail.com>
2024-10-09 23:09:35 +0200

    prompt edit

--- prompt.sh
+++ prompt.sh
@@ -16,16 +16,16 @@ function precmd () {
     VENV="%F{13}$VENV%f|"
   fi
 
-  export PROMPT="[$VENV%F{14}%n%f:%F{10}%3~%f$GIT]%% "
-  export RPROMPT="[%F{14}%m%f|%F{10}%T%f]"
+  export PROMPT="$VENV%F{14}%n%f:%F{10}%3~%f$GIT%% "
+  export RPROMPT="%F{14}%m%f|%F{10}%T%f"
 
   if [[ $COLUMNS -lt 101 ]]; then
-    export PROMPT="[$VENV%F{14}%n%f:%F{10}%c%f$GIT]%% "
-    export RPROMPT="[%F{14}%m%f|%F{10}%T%f]"
+    export PROMPT="$VENV%F{14}%n%f:%F{10}%c%f$GIT%% "
+    export RPROMPT="%F{14}%m%f|%F{10}%T%f"
   fi
 
   if [[ $COLUMNS -lt 71 ]]; then
-    export RPROMPT="[%F{10}%T%f]"
+    export RPROMPT="%F{10}%T%f"
   fi
 
   NEWLINE=$'\n'
