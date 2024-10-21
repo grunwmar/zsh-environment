71a81aee8e43a97359452cb87cb3115545682a60
martin grunwald <mgrunwald91@gmail.com>
2024-10-21 22:40:09 +0200

    #commit: Mon 21 Oct 21:31:35 CEST 2024

--- prompt2.sh
+++ prompt2.sh
@@ -9,18 +9,18 @@ function precmd () {
 
   GIT=$(get_git_branch)
   if ! [[ -z $GIT ]]; then
-    GIT=" %F{5}⌥ $GIT%f "
+    GIT=" %F{5} $GIT%f "
   fi
 
   VENV=$(basename "$VIRTUAL_ENV")
   if ! [[ -z $VENV ]]; then
-    VENV=" %F{6}▣ $VENV%f "
+    VENV=" %F{6} $VENV%f "
   fi
 
-  PROMPT_SIGN=" %F{15}%B%(!.#.%%)%b%f"
-  PREV_CMD_STATUS_VALUE="%(?.. %F{1}▼ %!%f)"
+  PROMPT_SIGN="%F{15}%(!.#.)%f"
+  PREV_CMD_STATUS_VALUE="%(?..%F{1} %!%f)"
   TIME=" %T"
-  USER_NAME="%(!.%F{11}%B!%f%F{5}%n%f%b.%F{10}%n%f)"
+  USER_NAME="%(!.%F{11}%B%f%F{5}%n%f%b.%F{10}%n%f)"
   MACHINE_NAME="%F{2}%m%f"
   CURRENT_DIR="%3~"
 
@@ -84,7 +84,7 @@ function precmd () {
     RPROMPT="$PREV_CMD_STATUS_VALUE $RPROMPT"
   fi
 
-  export PROMPT="$PROMPT "
+  export PROMPT="$PROMPT$PROMPT_SIGN "
   export RPROMPT="$RPROMPT"
 }
 
